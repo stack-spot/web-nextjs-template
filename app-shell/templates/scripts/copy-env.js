6671d311dce3f16c3a8f36dbeb1e4667dfdb5524
StackSpot Admin <admin@stackspot.com>
2022-09-06 19:15:17 +0000

    Update public repository

---
+++ app-shell/templates/scripts/copy-env.js
@@ -0,0 +1,34 @@
+const fs = require('fs/promises')
+
+async function updateEnv() {
+  let fileContent
+
+  try {
+    fileContent = await fs.readFile('./.env.template', { encoding: 'utf8' })
+    fileContent = fileContent.split(/\r?\n/)
+  } catch (err) {
+    throw new Error('Env template not found')
+  }
+
+  let envFile
+  try {
+    envFile = await fs.readFile('./.env', { encoding: 'utf8' })
+    envFile =  envFile.split(/\r?\n/)
+  } catch { }
+
+  if(fileContent) {
+    const linesToAdd = fileContent.filter((line) => {
+      return !envFile || envFile.indexOf(line) === -1
+    })
+    fs.writeFile('./.env', linesToAdd.join('\r\n'), { flag: 'a' }, err => {
+      if (err) {
+        console.error(err);
+      } else{
+        console.log('File .env updated.')
+      }
+    })
+  }
+
+}
+
+updateEnv()
